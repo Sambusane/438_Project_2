d209f55ac0fe57638b63b9e935ce849a8d0aadad
Oscar <olara@gmail.com>
2021-03-04 01:55:01 -0800

    Add test home page and route for adding items

--- wishlisht-frontend/src/Home.js
+++ wishlisht-frontend/src/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <div className="home">
-            <h1> Welcome </h1>
-            <h2> Log in or Sign up</h2>
+            <h1>Welcome</h1>
+            <h2>Log in or Sign up</h2>
         </div>
       );
 }
