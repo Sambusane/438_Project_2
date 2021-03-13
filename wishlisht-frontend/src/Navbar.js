823d1637b35287d21a0db1e0915e6675089c1fbb
Sam Busane <71896422+Sambusane@users.noreply.github.com>
2021-03-13 13:19:56 -0800

    login now changes the parent state and then passes the username to the navbar to display. (#13)
    Co-authored-by: Sbusane <sambusane@yahoo.com>

--- wishlisht-frontend/src/Navbar.js
+++ wishlisht-frontend/src/Navbar.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
-const Navbar = () => {
+const Navbar = (props) => {
+    const username = props.uName
     return (
         <nav className="navbar">
             <h1>Wish List</h1>
             <div className="links">
                 <Link to="/"> Home </Link>
                 <Link to="/test"> Test </Link>
+                  {username}
             </div>
         </nav>
       );
