595e086d2c3b92edbfa86336d54912dadb8bcbd6
Adrian Ortiz <adortiz@csumb.edu>
2021-03-23 21:58:58 -0700

    added a Link function for itemList

--- wishlisht-frontend/src/item.js
+++ wishlisht-frontend/src/item.js
@@ -1,17 +1,33 @@
 import React from "react";
+import * as url from "url";
+import {Link} from "react-router-dom";
 
 class item extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             name: this.props.name,
-            price: this.props.price
+            price: this.props.price,
+            image: this.props.image,
+            url : this.props.url
         }
     }
 
     render(){
         return(
-            <div className="items">{this.state.name }</div>
+            <div className="items">
+                {this.state.name }
+                {this.state.price}
+                {/*<button onClick= href = "google.com">*/}
+                {/*    Click here*/}
+                {/*</button>*/}
+
+                <Link
+                href = "http://google.com/">
+                    Link
+                </Link>
+            </div>
+
         )
     }
 }
